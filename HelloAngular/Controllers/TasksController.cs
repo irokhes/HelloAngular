2942f8ca8a810f319d2835b2f46dea033a612279
irokhes <jorgereyero@gmail.com>
2014-06-25 21:56:02 +0100

    Add mvc solution

---
+++ HelloAngular/Controllers/TasksController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+
+namespace HelloAngular.Controllers
+{
+    public class TasksController : ApiController
+    {
+        static List<Task> tasks = new List<Task>()
+        {
+            new Task{ Id = 1, Name = "Learn Angular.js", Status = TaskStatus.Pending.ToString()},
+            new Task{ Id = 2, Name = "Learn Node.js", Status = TaskStatus.Pending.ToString()},
+            new Task{ Id = 1, Name = "Write a book", Status = TaskStatus.Pending.ToString()}
+        };
+
+        public IEnumerable<Task> Get()
+        {
+            return tasks;
+        }
+
+        public Task Get(int id)
+        {
+            return tasks.Single(x => x.Id == id);
+        }
+
+        private static int maxId = 3;
+
+        public void Post([FromBody] Task value)
+        {
+            var header = HttpContext.Current.Request.Headers;
+            IEnumerable<string> headerValues = header.GetValues("CommandType");
+            var commandType = headerValues.FirstOrDefault();
+            if(commandType != "CreateTask")
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+
+            value.Id = ++maxId;
+            value.Status = TaskStatus.Pending.ToString();
+            tasks.Add(value);
+            HttpContext.Current.Response.AddHeader("Location","api/tasks/" + value.Id);
+        }
+
+        
+
+        
+    }
+
+
+
+    public class Task 
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Status { get; set; }
+    }
+
+    public enum TaskStatus
+    {
+        Undefinied = 0,
+        Pending = 1,
+        Started = 2,
+        Done = 3,
+        Cancelled = 4
+    }
+}
---
+++ HelloAngular/Scripts/step5.js
@@ -0,0 +1,33 @@
+﻿/// <reference path="~/Scripts/angular.js"/>
+var app = angular.module('myApp', []);
+
+app.controller('TaskListCtrl', function($scope, $http) {
+    loadData();
+
+    function loadData() {
+        $http.get("api/tasks").success(function (data) {
+            $scope.tasks = data;
+        });
+    };
+
+    $scope.refresh = function() {
+        loadData();
+    };
+});
+ 
+app.controller('NewTaskCtrl', function($scope, $http) {
+    $scope.taskName = null;
+
+    $scope.createTask = function () {
+        var config = {
+            headers: { "CommandType": "CreateTask" }
+        };
+        var data = { Name: $scope.taskName };
+        $http.post('api/tasks', data, config).success(function(data, status, headers) {
+            alert("Task added");
+            $http.get(headers("Location")).success(function(data) {
+                $scope.tasks.push(data);
+            });
+        });
+    };
+});
