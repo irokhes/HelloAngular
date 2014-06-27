f981da23ea864781d228e8016a9172b243ef258f
irokhes <jorgereyero@gmail.com>
2014-06-27 21:44:47 +0100

    step6 finished

--- HelloAngular/Scripts/step6.js
+++ HelloAngular/Scripts/step6.js
@@ -4,7 +4,7 @@ var app = angular.module('myAppStep6', []);
 app.controller('QuestionnaireCtrl', function ($scope) {
     $scope.questionnaire = [
         { number: 1, question: "How old are you?", type: "number" },
-        { number: 1, question: "You date of birth?", type: "data" },
+        { number: 1, question: "You date of birth?", type: "date" },
         { number: 1, question: "Describe yourself in a few words", type: "text" },
         { number: 1, question: "Which is your favorite colour?", type: "singleOption" },
         { number: 1, question: "Which of the following sweets do you like?", type: "multiOption" }
